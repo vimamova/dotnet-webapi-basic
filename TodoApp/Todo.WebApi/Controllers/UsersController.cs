edcfe19dddb4a5de1693c8f2cdab53cda36ea9c1
Viviana <vimamova07@gmail.com>
2025-11-18 23:56:14 -0500

    first commit

---
+++ TodoApp/Todo.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public UsersController()
+        {
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> GelHelloWorld()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
---
+++ TodoApp/Todo.WebApi/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting();
+app.UseEndpoints(Endpoint =>
+{
+    Endpoint.MapControllers();
+}
+);
+
+app.UseHttpsRedirection();
+
+app.Run();
